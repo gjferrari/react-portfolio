90d260c3ade64cbd3ed1af06f8f06ab473fef206
Genevieve Ferrari <genferrari@gmail.com>
2023-03-02 00:49:16 -0800

    setting up code to show 6 projects

---
+++ src/components/project-card.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card, Badge, Button } from "react-bootstrap";
+
+export function projectCard({data, setOrdered}){
+    return(
+        <Card className="h-100 shadow-sm bg-white rounded"></Card>
+    )
+}
---
+++ src/components/project.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+// import projects from "../data.json";
+
+const Projects = () => {};
+
+export default Projects;
